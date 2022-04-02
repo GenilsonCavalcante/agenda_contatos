58ed07d3887a9f62c7ff393327bf4e12f80c3a1f
GenilsonCavalcante <cavalcantedeoliveiragenilson@gmail.com>
2022-04-02 17:29:07 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+   include_once("templates/header.php");
+?>
+<main class="main">
+   <h1>Minha Agenda</h1>
+</main>
+<?php
+   include_once("templates/footer.php");
+?>
---
+++ templates/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Agenda</title>
+
+   <!-- CSS -->
+   <link rel="stylesheet" href="css/reset.css">
+   <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+   <header class="header">
+      <nav class="header__nav">
+         <a href="#" class="header__link">
+            <img src="img/logo.svg" class="header__logo" alt="Logo agenda de contatos">
+         </a>
+         <div class="header__link-actions">
+            <a href="#" class="header__link">Agenda</a>
+            <a href="#" class="header__link">Adicionar Contato</a>
+         </div>
+      </nav>
+   </header>
